f44eedf4c79d09df4091b903a6c57c75a8fc71f5
saqib867 <saqibhussain9910@gmail.com>
2024-03-14 21:52:20 +0500

    first task has been adjusted

--- routes/getLink.js
+++ routes/getLink.js
@@ -8,54 +8,54 @@ const router = express.Router()
 
 
 // method number first
-router.get('/title1',async(req,res)=>{
-  const parsedUrl = url.parse(req.url, true);
-  const {query } = parsedUrl;
-
-  
-    let addresses = query.address;
-    if (!Array.isArray(addresses)) {
-      addresses = [addresses];
-    }
-    
-    if (addresses.some(address => typeof address !== 'string' || address.trim() === '')) {
-      res.writeHead(400, { 'Content-Type': 'text/plain' });
-      res.end('400 Bad Request: Invalid address');
-      return;
-    }
-
-    const promises = addresses.map(async address => {
-      if (!address.includes('http')) {
-        address = 'http://' + address;
-      }
-      try {
-        const response = await axios.get(address)
-        const title = response.data.match(/<title>(.*?)<\/title>/i)
-        return title ? title[1] : 'NO RESPONSE'
-      } catch (error) {
-        console.error(`Error fetching ${address}: ${error.message}`)
-        return 'NO RESPONSE'
-      }
-    });
-
-    Promise.all(promises)
-    .then(titles => {
-      let htmlResponse = '<html><head></head><body><h1>Following are the titles of given websites:</h1><ul>';
-      addresses.forEach((address, index) => {
-        htmlResponse += `<li>${address} - "${titles[index]}"</li>`;
-      });
-      htmlResponse += '</ul></body></html>';
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(htmlResponse);
-    })
-    .catch(error => {
-      console.error(`Error processing requests: ${error.message}`);
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Internal Server Error');
-    });
+router.get('/title1', (req, res) => {
  
-  
-})
+
+  let addresses = req.query.address;
+  if (!Array.isArray(addresses)) {
+    addresses = [addresses];
+  }
+
+  if (addresses.some(address => typeof address !== 'string' || address.trim() === '')) {
+    res.writeHead(400, { 'Content-Type': 'text/plain' });
+    res.end('400 Bad Request: Invalid address');
+    return;
+  }
+
+  const responses = [];
+  let completedCount = 0;
+
+  addresses.forEach((address, index) => {
+    if (!address.includes('http')) {
+      address = 'http://' + address;
+    }
+    axios.get(address)
+      .then(response => {
+        const title = response.data.match(/<title>(.*?)<\/title>/i);
+        responses[index] = title ? title[1] : 'NO RESPONSE';
+        completedCount++;
+
+        if (completedCount === addresses.length) {
+          sendResponse();
+        }
+      })
+      .catch(error => {
+        console.error(`Error fetching ${address}: ${error.message}`);
+        responses[index] = 'NO RESPONSE';
+       
+      });
+  });
+
+  function sendResponse() {
+    let htmlResponse = '<html><head></head><body><h1>Following are the titles of given websites:</h1><ul>';
+    addresses.forEach((address, index) => {
+      htmlResponse += `<li>${address} - "${responses[index]}"</li>`;
+    });
+    htmlResponse += '</ul></body></html>';
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(htmlResponse);
+  }
+});
 
 // method number 2
 
@@ -81,7 +81,9 @@ router.get('/title2',async(req,res)=>{
       }
       axios.get(address)
         .then(response => {
+          
           const title = response.data.match(/<title>(.*?)<\/title>/i);
+          console.log("title => ",title)
           callback(null, title ? title[1] : 'NO RESPONSE');
         })
         .catch(error => {
